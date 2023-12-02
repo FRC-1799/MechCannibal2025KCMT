fdc2ba37281e889993a936098a08503b7a6bbb9c
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2023-12-01 17:18:45 -0700

    fixed an issue

--- src/main/java/frc/robot/commands/auto/AutoRoutines/Auto_DumpandGo.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutines/Auto_DumpandGo.java
@@ -7,8 +7,9 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 //import frc.robot.Constants;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.commands.auto.AutoFunctions.DriveStraight;
+import frc.robot.Constants;
 import frc.robot.commands.ArmMove;
-import frc.robot.commands.ArmMove.arm;
+import frc.robot.subsystems.ArmSubsystem;
 
 public class Auto_DumpandGo extends SequentialCommandGroup {
   /*
@@ -20,23 +21,20 @@ public class Auto_DumpandGo extends SequentialCommandGroup {
 
   // Subsystem to Dump Cargo then go forward over charge station
   // and then back up onto charge system to attempt balance
-  public Auto_DumpandGo(Arm arm, DriveBase drive, DriveStraight execute) {
+  public Auto_DumpandGo(ArmSubsystem arm, DriveBase drive, DriveStraight execute) {
     super(
       new WaitCommand(1),
       //dump game piece
-      new InstantCommand(
-        ()->arm.move(),
-        arm
+    new ArmMove(arm, null),
         /*place holder for now 
          *Owen will fix ArmMove once he does make sure to come back
          *fix line 28 so its not bolded
          */
-      ),
-      new WaitCommand(3),
+      
+      new WaitCommand(5),
 
-      new InstantCommand(
-        DriveStraight(drive, 3)
-      ),
+      new DriveStraight(drive, 3.0, Constants.auto.fwdSpeed, false)
+      
       
     );
   }
