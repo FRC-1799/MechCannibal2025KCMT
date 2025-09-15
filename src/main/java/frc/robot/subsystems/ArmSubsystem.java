8eaecc5c85a64c042764c80b1ebb278f39d64850
ThePrinterFRC1799 <frc1799printer@gmail.com>
2025-09-15 16:19:26 -0600

    updates vendor deps

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,94 +1,105 @@
 package frc.robot.subsystems;
 
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+import static yams.mechanisms.SmartMechanism.gearbox;
+import static yams.mechanisms.SmartMechanism.gearing;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import yams.mechanisms.config.ArmConfig;
+import yams.mechanisms.config.MechanismPositionConfig;
+import yams.mechanisms.positional.Arm;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
-public class ArmSubsystem extends SubsystemBase {
+public class ArmSubsystem extends SubsystemBase
+{
 
-  public final WPI_VictorSPX armOne = new WPI_VictorSPX(Constants.arm.ArmOnePort);
-  public final WPI_VictorSPX armTwo = new WPI_VictorSPX(Constants.arm.ArmTwoPort);
-  public final DigitalInput topSwitch = new DigitalInput(Constants.arm.topSwitch);
-  public final DigitalInput bottomSwitch = new DigitalInput(Constants.arm.bottomSwitch);
-  public boolean isGoingDown = false;
-  public boolean isActive = false;
-  final MotorControllerGroup armMotors = new MotorControllerGroup(armOne, armTwo);
+  private final SparkMax                   armMotor         = new SparkMax(1, MotorType.kBrushless);
+  //  private final SmartMotorControllerTelemetryConfig motorTelemetryConfig = new SmartMotorControllerTelemetryConfig()
+//          .withMechanismPosition()
+//          .withRotorPosition()
+//          .withMechanismLowerLimit()
+//          .withMechanismUpperLimit();
+  private final SmartMotorControllerConfig motorConfig = new SmartMotorControllerConfig(this)
+      .withClosedLoopController(4, 0, 0, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
+      .withSoftLimit(Degrees.of(-30), Degrees.of(100))
+      .withGearing(gearing(gearbox(3, 4)))
+//      .withExternalEncoder(armMotor.getAbsoluteEncoder())
+      .withIdleMode(MotorMode.BRAKE)
+      .withTelemetry("ArmMotor", TelemetryVerbosity.HIGH)
+//      .withSpecificTelemetry("ArmMotor", motorTelemetryConfig)
+      .withStatorCurrentLimit(Amps.of(40))
+//      .withVoltageCompensation(Volts.of(12))
+      .withMotorInverted(false)
+      .withClosedLoopRampRate(Seconds.of(0.25))
+      .withOpenLoopRampRate(Seconds.of(0.25))
+      .withFeedforward(new ArmFeedforward(0, 0, 0, 0))
+      .withControlMode(ControlMode.CLOSED_LOOP);
+  private final SmartMotorController       motor            = new SparkWrapper(armMotor,
+                                                                               DCMotor.getNEO(1),
+                                                                               motorConfig);
+  private final MechanismPositionConfig    robotToMechanism = new MechanismPositionConfig()
+      .withMaxRobotHeight(Meters.of(1.5))
+      .withMaxRobotLength(Meters.of(0.75))
+      .withRelativePosition(new Translation3d(Meters.of(0.25), Meters.of(0), Meters.of(0.5)));
 
 
-  public void move() {
-    printInfo();
-    if(isActive){
-      if (!isGoingDown){
-        if (!topSwitch.get()) {
-          isGoingDown = true;
-          armMotors.set(Constants.arm.ArmDown);
-          
-          // We are going up and top limit is tripped so stop
-        }
-        else{
-          armMotors.set(Constants.arm.ArmUp);
-        }
-      }
-      else{
-        if (!bottomSwitch.get() && isGoingDown) {
-          // We are going down and bottom limit is tripped so stop
-        
-          armMotors.set(0);
-          isGoingDown = false;
-          isActive = false;
-          
-      }
-        else{
-          armMotors.set(Constants.arm.ArmDown);
-      }
-    }
+  private       ArmConfig m_config = new ArmConfig(motor)
+      .withLength(Meters.of(0.135))
+      .withHardLimit(Degrees.of(-100), Degrees.of(200))
+      .withTelemetry("ArmExample", TelemetryVerbosity.HIGH)
+      .withMass(Pounds.of(1))
+      .withStartingPosition(Degrees.of(0))
+      .withHorizontalZero(Degrees.of(0))
+      .withMechanismPositionConfig(robotToMechanism);
+  private final Arm       arm      = new Arm(m_config);
+
+  public ArmSubsystem()
+  {
+  }
+
+  public void periodic()
+  {
+    arm.updateTelemetry();
+  }
+
+  public void simulationPeriodic()
+  {
+    arm.simIterate();
+  }
+
+  public Command armCmd(double dutycycle)
+  {
+    return arm.set(dutycycle);
+  }
+
+  public Command sysId()
+  {
+    return arm.sysId(Volts.of(3), Volts.of(3).per(Second), Second.of(30));
+  }
+
+  public Command setAngle(Angle angle)
+  {
+    return arm.setAngle(angle);
   }
 }
-  // public void setActive() {
-  //   isActive = true;
-  // }
-  private void printInfo(){
-    SmartDashboard.putBoolean("isup", isGoingDown);
-    SmartDashboard.putBoolean("topSwitch", topSwitch.get());
-    SmartDashboard.putBoolean("botttomSwitch", bottomSwitch.get());
-    SmartDashboard.putBoolean("isActive", isActive);
-  }
-  
-  public void moveManuel(double speed){
-    armMotors.set(speed);
-  }
-
-}
-  // public void move2(DigitalInput endSwitch, MotorControllerGroup motor){
-  //   while (endSwitch.get()){
-  //     motor.set(constants.arm.)
-  //   }
-  // }
-    // SmartDashboard.putBoolean("isUp", isUp);
-    // while ( topSwitch.get() || bottomSwitch.get()){
-    //   if (isUp){
-    //       armMotors.set(Constants.arm.ArmDown);
-          
-    //   }
-    //   else{
-    //     armMotors.set(Constants.arm.ArmUp);
-        
-
-    //   }                                                       
-    // }
-    // armMotors.set(0);
-    // if (isUp){
-    //   isUp=false;
-    // }
-    // else{
-    //   isUp=true;
-    // }
-
-
- 
-
-
